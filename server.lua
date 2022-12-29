d51688885602382b8476ebab7831494dc71ffae0
Tkiljoy <50683104+Tkiljoy@users.noreply.github.com>
2022-12-29 00:57:24 -0600

    fix for peyote plants not working

--- server.lua
+++ server.lua
@@ -27,30 +27,30 @@ QBCore.Functions.CreateUseableItem("bluepeyoteplant", function(source, item)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items[item], "remove")
-    TriggerServerEvent("QBCore:Server:RemoveItem", item, 1)
+    Player.Functions.RemoveItem("bluepeyoteplant",1)
     TriggerClientEvent("tk-peyote:client:bluepeyote", src)
 end)
 
-QBCore.Functions.CreateUseableItem("pinkpeyote", function(source, item)
+QBCore.Functions.CreateUseableItem("pinkpeyoteplant", function(source, item)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items[item], "remove")
-    TriggerServerEvent("QBCore:Server:RemoveItem", item, 1)
+    Player.Functions.RemoveItem("pinkpeyoteplant",1)
     TriggerClientEvent("tk-peyote:client:pinkpeyote", src)
 end)
 
-QBCore.Functions.CreateUseableItem("whitepeyote", function(source, item)
+QBCore.Functions.CreateUseableItem("whitepeyoteplant", function(source, item)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items[item], "remove")
-    TriggerServerEvent("QBCore:Server:RemoveItem", item, 1)
+    Player.Functions.RemoveItem("whitepeyoteplant",1)
     TriggerClientEvent("tk-peyote:client:whitepeyote", src)
 end)
 
-QBCore.Functions.CreateUseableItem("yellowpeyote", function(source, item)
+QBCore.Functions.CreateUseableItem("yellowpeyoteplant", function(source, item)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items[item], "remove")
-    TriggerServerEvent("QBCore:Server:RemoveItem", item, 1)
+    Player.Functions.RemoveItem("yellowpeyoteplant",1)
     TriggerClientEvent("tk-peyote:client:yellowpeyote", src)
 end)
